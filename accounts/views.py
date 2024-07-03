a9dc69d6eb8b0394f55e3df1e3ffbb0fb2736fbc
mark <markopaul909@gmail.com>
2024-07-03 15:02:39 +0300

    A2

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,179 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+from django.core.validators import validate_email
+from django.http import HttpResponse, HttpResponseRedirect, HttpResponseNotAllowed, JsonResponse
+from django.contrib.auth import authenticate, login, logout, update_session_auth_hash
+from django.contrib.auth import update_session_auth_hash
+from django.template.response import TemplateResponse
+from django.views import View
 
 # Create your views here.
+
+class Register(View):
+
+    def get(self, request, *args, **kwargs):
+        return TemplateResponse(request, 'accounts/register.html')
+
+    def post(self, request, *args, **kwargs):
+
+        password1 = request.POST.get('password1', '')
+        password2 = request.POST.get('password2', '')
+        username = request.POST.get('username', '')
+        email = request.POST.get('email', '')
+        first_name = request.POST.get('first_name', '')
+        last_name = request.POST.get('last_name', '')
+
+        context = {'username': [], 'password1': [], 'password2': [], 'email': [], 'username_value': username,
+                   'email_value': email, 'first_name_value': first_name, 'last_name_value': last_name}
+
+        if password1 != password2:
+            context['password1'].append("The two password fields didn't match")
+            context['password2'].append("The two password fields didn't match")
+
+        if len(password1) < 8:
+            context['password1'].append("This password is too short. It must contain at least 8 characters")
+        if password1 is None:
+            context['password1'].append("This field is required")
+
+        if len(password2) < 8:
+            context['password2'].append("This password is too short. It must contain at least 8 characters")
+        if password2 is None:
+            context['password2'].append("This field is required")
+
+        if username is None:
+            context['username'].append("This field is required")
+
+        try:
+            User.objects.get(username=username)
+        except User.DoesNotExist:
+            pass
+        else:
+            context['username'].append("A user with that username already exists")
+
+        try:
+            validate_email(email)
+        except ValidationError:
+            context['email'].append("Enter a valid email address")
+        else:
+            pass
+
+        if len(context['username']) > 0 or len(context['password1']) > 0 or len(context['password2']) > 0 or len(
+                context['email']) > 0:
+            return TemplateResponse(request, 'accounts/register.html',
+                                    context=context)
+        else:
+            User.objects.create_user(username=username, password=password1, email=email, first_name=first_name,
+                                     last_name=last_name)
+
+            return HttpResponseRedirect('/accounts/login/')
+
+    HttpResponseNotAllowed(['GET', 'POST'])
+
+
+class Login(View):
+
+    def get(self, request, *args, **kwargs):
+        return TemplateResponse(request, 'accounts/login.html')
+
+    def post(self, request, *args, **kwargs):
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        context = {'errors': [], "username_value": username}
+
+        if user := authenticate(username=username, password=password):
+            login(request, user)
+            return HttpResponseRedirect('/accounts/profile/view/')
+        else:
+            context['errors'].append('Username or password is invalid')
+            return TemplateResponse(request, 'accounts/login.html', context=context)
+
+    HttpResponseNotAllowed(['GET', 'POST'])
+
+
+class Logout(View):
+    def get(self, request, *args, **kwargs):
+        logout(request)
+        return HttpResponseRedirect('/accounts/login/')
+
+    HttpResponseNotAllowed(['GET'])
+
+
+class ProfileView(View):
+    def get(self, request, *args, **kwargs):
+
+        if request.user.is_authenticated:
+            user = request.user
+
+            data = {"id": user.id, "username": user.username, "email": user.email, "first_name": user.first_name,
+                    "last_name": user.last_name}
+            return JsonResponse(data)
+        else:
+            return HttpResponse('401 UNAUTHORIZED ', status=401)
+
+    HttpResponseNotAllowed(['GET'])
+
+
+class EditProfile(View):
+
+    def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            user = request.user
+
+            context = {'email_value': user.email, 'first_name_value': user.first_name,
+                       'last_name_value': user.last_name}
+            return TemplateResponse(request, 'accounts/edit_profile.html',
+                                    context=context)
+        else:
+            return HttpResponse('401 UNAUTHORIZED ', status=401)
+
+    def post(self, request, *args, **kwargs):
+
+        if request.user.is_authenticated:
+            user = request.user
+
+            # updated values
+            password1 = request.POST.get('password1', '')
+            password2 = request.POST.get('password2', '')
+            email = request.POST.get('email', '')
+            first_name = request.POST.get('first_name', '')
+            last_name = request.POST.get('last_name', '')
+
+            user.first_name = first_name
+            user.last_name = last_name
+
+            context = {'password1': [], 'password2': [], 'email': [],
+                       'email_value': email, 'first_name_value': first_name, 'last_name_value': last_name}
+
+            try:
+                validate_email(email)
+            except ValidationError:
+                context['email'].append("Enter a valid email address")
+            else:
+                user.email = email
+
+            if password1 != password2:
+                context['password1'].append("The two password fields didn't match")
+                context['password2'].append("The two password fields didn't match")
+
+            if (password1 is None or password1 is "") and (password2 is None or password2 is ""):
+                pass
+            if 1 <= len(password1) < 8:
+                context['password1'].append("This password is too short. It must contain at least 8 characters")
+            if 1 <= len(password2) < 8:
+                context['password2'].append("This password is too short. It must contain at least 8 characters")
+            else:
+                user.set_password(str(password1))
+
+            if len(context['password1']) > 0 or len(context['password2']) > 0 or len(context['email']) > 0:
+                return TemplateResponse(request, 'accounts/edit_profile.html',
+                                        context=context)
+            else:
+                user.save()
+                update_session_auth_hash(request, user)
+                return HttpResponseRedirect('/accounts/profile/view/')
+
+        else:
+            return HttpResponse('401 UNAUTHORIZED ', status=401)
+
+    HttpResponseNotAllowed(['GET', 'POST'])
--- banks/views.py
+++ banks/views.py
@@ -1,3 +1,366 @@
-from django.shortcuts import render
+from django.core.exceptions import ValidationError
+from django.core.validators import validate_email
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse, HttpResponseNotAllowed
+from django.template.response import TemplateResponse
+from django.views import View
+from django.views.generic import ListView
+from banks.models import Bank, Branch
+
 
 # Create your views here.
+
+class AddBank(View):
+
+    def get(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return TemplateResponse(request, 'banks/add_Bank.html')
+        else:
+            return HttpResponse('401 UNAUTHORIZED', status=401)
+
+    def post(self, request, *args, **kwargs):
+
+        if request.user.is_authenticated:
+            pass
+        else:
+            return HttpResponse('401 UNAUTHORIZED', status=401)
+
+        name = request.POST.get('name', '')
+        description = request.POST.get('description', '')
+        inst_num = request.POST.get('inst_num', '')
+        swift_code = request.POST.get('swift_code', '')
+
+        context = {'name': [], 'description': [], 'inst_num': [], 'swift_code': [], 'name_value': name,
+                   'description_value': description, 'inst_num_value': inst_num, 'swift_code_value': swift_code}
+
+        if name is None or name == "":
+            context['name'].append('This field is required')
+        elif len(name) > 100:
+            context['name'].append("Ensure this value has at most 100 characters (it has " + str(len(name)) + ")")
+
+        if description is None or description == "":
+            context['description'].append('This field is required')
+        elif len(description) > 100:
+            context['description'].append(
+                "Ensure this value has at most 100 characters (it has " + str(len(description)) + ")")
+
+        if inst_num is None or inst_num == "":
+            context['inst_num'].append('This field is required')
+        elif len(inst_num) > 100:
+            context['inst_num'].append(
+                "Ensure this value has at most 100 characters (it has " + str(len(inst_num)) + ")")
+
+        if swift_code is None or swift_code == "":
+            context['swift_code'].append('This field is required')
+        elif len(swift_code) > 100:
+            context['swift_code'].append(
+                "Ensure this value has at most 100 characters (it has " + str(len(swift_code)) + ")")
+
+        if len(context['name']) > 0 or len(context['description']) > 0 or len(context['inst_num']) > 0 or len(
+                context['swift_code']) > 0:
+            return TemplateResponse(request, 'banks/add_Bank.html',
+                                    context=context)
+        else:
+            user = request.user
+            bank = Bank.objects.create(name=name, description=description, inst_num=inst_num, swift_code=swift_code,
+                                       owner=user)
+            bank_id = bank.id
+            return HttpResponseRedirect(f'/banks/{bank_id}/details/')
+
+    HttpResponseNotAllowed(['GET', 'POST'])
+
+
+class AddBranch(View):
+
+    def get(self, request, *args, **kwargs):
+
+        user_logged_in = request.user
+        url_bank_id = self.kwargs['bank_id']
+
+        # Check if user is logged in
+        if request.user.is_authenticated:
+            pass
+        else:
+            return HttpResponse('401 UNAUTHORIZED', status=401)
+
+        # context = {'email_value': Branch.get_field(Branch.email).get_default()}
+        context = {'email_value': 'admin@utoronto.ca'}
+
+        # Check if bank id exists
+        if Bank.objects.filter(id=url_bank_id).exists():
+            bank = Bank.objects.get(id=url_bank_id)
+            bank_owner = bank.owner
+        else:
+            return HttpResponse('404 NOT FOUND', status=404)
+
+        # Check if logged in user is the owner of the bank
+        if user_logged_in.id == bank_owner.id:
+            pass
+        else:
+            return HttpResponse('403 FORBIDDEN', status=403)
+
+        return TemplateResponse(request, 'banks/add_Branch.html', context=context)
+
+    def post(self, request, *args, **kwargs):
+
+        user_logged_in = request.user
+        url_bank_id = self.kwargs['bank_id']
+
+        # Check if user is logged in
+        if request.user.is_authenticated:
+            pass
+        else:
+            return HttpResponse('401 UNAUTHORIZED', status=401)
+
+        # Check if bank id exists
+        if Bank.objects.filter(id=url_bank_id).exists():
+            bank = Bank.objects.get(id=url_bank_id)
+            bank_owner = bank.owner
+        else:
+            return HttpResponse('404 NOT FOUND', status=404)
+
+        # Check if logged in user is the owner of the bank
+        if user_logged_in.id == bank_owner.id:
+            pass
+        else:
+            return HttpResponse('403 FORBIDDEN', status=403)
+
+        name = request.POST.get('name', '')
+        transit_num = request.POST.get('transit_num', '')
+        address = request.POST.get('address', '')
+        email = request.POST.get('email', '')
+        capacity = request.POST.get('capacity', '')
+
+        context = {'name': [], 'transit_num': [], 'address': [], 'email': [], 'capacity': [],
+                   'name_value': name, 'transit_num_value': transit_num, 'address_value': address,
+                   'email_value': email}
+
+        if name is None or name == "":
+            context['name'].append('This field is required')
+        elif len(name) > 100:
+            context['name'].append("Ensure this value has at most 100 characters (it has " + str(len(name)) + ")")
+
+        if transit_num is None or transit_num == "":
+            context['transit_num'].append('This field is required')
+        elif len(transit_num) > 100:
+            context['transit_num'].append(
+                "Ensure this value has at most 100 characters (it has " + str(len(transit_num)) + ")")
+
+        if address is None or address == "":
+            context['address'].append('This field is required')
+        elif len(address) > 100:
+            context['address'].append("Ensure this value has at most 100 characters (it has " + str(len(address)) + ")")
+
+        if email is None or email == '':
+            context['email'].append('This field is required')
+        try:
+            validate_email(email)
+        except ValidationError:
+            context['email'].append("Enter a valid email address")
+        else:
+            pass
+
+        if capacity is None or capacity == "":
+            context['capacity_value'] = None
+        elif int(capacity) < 0:
+            context['capacity'].append("Ensure this value is greater than or equal to 0")
+            context['capacity_value'] = capacity
+
+        if len(context['name']) > 0 or len(context['transit_num']) > 0 or len(context['address']) > 0 or len(
+                context['email']) > 0 or len(context['capacity']) > 0:
+            return TemplateResponse(request, 'banks/add_Branch.html',
+                                    context=context)
+        else:
+            user = request.user
+
+            if capacity is None or capacity == "":
+                capacity = None
+            else:
+                capacity = int(capacity)
+
+            branch = Branch.objects.create(name=name, transit_num=transit_num, address=address, email=email,
+                                           capacity=capacity, bank=bank)
+            branch_id = branch.id
+            return HttpResponseRedirect(f'/banks/branch/{branch_id}/details/')
+
+    HttpResponseNotAllowed(['GET', 'POST'])
+
+
+class BankIdDetails(View):
+
+    def get(self, request, *args, **kwargs):
+        url_bank_id = self.kwargs['bank_id']
+
+        # Check if bank id exists
+        if Bank.objects.filter(id=url_bank_id).exists():
+            bank = Bank.objects.get(id=url_bank_id)
+
+            # get all branches of this bank
+            data = Branch.objects.filter(bank=bank).values()
+
+            context = {'id': url_bank_id, 'name': bank.name, 'description': bank.description,
+                       'swift_code': bank.swift_code, 'inst_num': bank.inst_num, 'data': data}
+
+            return TemplateResponse(request, 'banks/bank_Details.html', context=context)
+
+        else:
+            return HttpResponse('404 NOT FOUND', status=404)
+
+    HttpResponseNotAllowed(['GET'])
+
+
+class BranchIdDetails(View):
+
+    def get(self, request, *args, **kwargs):
+
+        url_branch_id = self.kwargs['branch_id']
+
+        # Check if user is logged in
+        if request.user.is_authenticated:
+            pass
+        else:
+            return HttpResponse('401 UNAUTHORIZED', status=401)
+
+        # Check if branch id exists
+        if Branch.objects.filter(id=url_branch_id).exists():
+            branch = Branch.objects.get(id=url_branch_id)
+        else:
+            return HttpResponse('404 NOT FOUND', status=404)
+
+        # get branch data
+        data = {"id": branch.id, "name": branch.name, "transit_num": branch.transit_num, "address": branch.address,
+                "email": branch.email, "capacity": branch.capacity, "last_modified": branch.last_modified}
+        return JsonResponse(data)
+
+    HttpResponseNotAllowed(['GET'])
+
+
+class AllBanks(ListView):
+    model = Bank
+    template_name = 'banks/bank_list.html'
+
+    def get(self, request, *args, **kwargs):
+        return super(AllBanks, self).get(request, *args, **kwargs)
+
+    HttpResponseNotAllowed(['GET'])
+
+class EditBranch(View):
+    def get(self, request, *args, **kwargs):
+        url_branch_id = self.kwargs['branch_id']
+
+        # Check if user is logged in
+        if request.user.is_authenticated:
+            pass
+        else:
+            return HttpResponse('401 UNAUTHORIZED', status=401)
+
+        # Check if branch id exists
+        if Branch.objects.filter(id=url_branch_id).exists():
+            branch = Branch.objects.get(id=url_branch_id)
+        else:
+            return HttpResponse('404 NOT FOUND', status=404)
+
+        # Check if user is the owner of the corresponding bank
+        bank = branch.bank
+        bank_owner = bank.owner
+        user_logged_in = request.user
+
+        if user_logged_in.id == bank_owner.id:
+            pass
+        else:
+            return HttpResponse('403 FORBIDDEN', status=403)
+
+        # data = Branch.objects.filter(id=url_branch_id).values()
+        context = {'name_value': branch.name, 'transit_num_value': branch.transit_num, 'address_value': branch.address,
+                   'email_value': branch.email, 'capacity_value': branch.capacity}
+
+        return TemplateResponse(request, 'banks/edit_Branch.html',
+                                context=context)
+
+    def post(self, request, *args, **kwargs):
+        url_branch_id = self.kwargs['branch_id']
+
+        # Check if user is logged in
+        if request.user.is_authenticated:
+            pass
+        else:
+            return HttpResponse('401 UNAUTHORIZED', status=401)
+
+        # Check if branch id exists
+        if Branch.objects.filter(id=url_branch_id).exists():
+            branch = Branch.objects.get(id=url_branch_id)
+        else:
+            return HttpResponse('404 NOT FOUND', status=404)
+
+        # Check if user that's logged in is the owner of the corresponding bank
+        bank = branch.bank
+        bank_owner = bank.owner
+        user_logged_in = request.user
+
+        if user_logged_in.id == bank_owner.id:
+            pass
+        else:
+            return HttpResponse('403 FORBIDDEN', status=403)
+
+        # updated values
+        name = request.POST.get('name', '')
+        transit_num = request.POST.get('transit_num', '')
+        address = request.POST.get('address', '')
+        email = request.POST.get('email', '')
+        capacity = request.POST.get('capacity', '')
+
+        context = {'name': [], 'transit_num': [], 'address': [], 'email': [], 'capacity': [], 'name_value': branch.name,
+                   'transit_num_value': branch.transit_num, 'address_value': branch.address,
+                   'email_value': branch.email, 'capacity_value': branch.capacity}
+
+        if name is None or name == "":
+            context['name'].append('This field is required')
+        elif len(name) > 100:
+            context['name'].append("Ensure this value has at most 100 characters (it has " + str(len(name)) + ")")
+        context['name_value'] = name
+
+        if transit_num is None or transit_num == "":
+            context['transit_num'].append('This field is required')
+        elif len(transit_num) > 100:
+            context['transit_num'].append(
+                "Ensure this value has at most 100 characters (it has " + str(len(transit_num)) + ")")
+        context['transit_num_value'] = transit_num
+
+        if address is None or address == "":
+            context['address'].append('This field is required')
+        elif len(address) > 100:
+            context['address'].append("Ensure this value has at most 100 characters (it has " + str(len(address)) + ")")
+        context['address_value'] = address
+
+        if email is None or email == '':
+            context['email'].append('This field is required')
+        try:
+            validate_email(email)
+        except ValidationError:
+            context['email'].append("Enter a valid email address")
+        context['email_value'] = email
+
+        if capacity is None or capacity == "":
+            context['capacity_value'] = None
+        elif int(capacity) < 0:
+            context['capacity'].append("Ensure this value is greater than or equal to 0")
+            context['capacity_value'] = capacity
+
+        if len(context['name']) > 0 or len(context['transit_num']) > 0 or len(context['address']) > 0 or len(
+                context['email']) > 0 or len(context['capacity']) > 0:
+
+            return TemplateResponse(request, 'banks/edit_Branch.html',
+                                    context=context)
+        else:
+            branch.name = name
+            branch.transit_num = transit_num
+            branch.address = address
+            branch.email = email
+            if capacity is None or capacity == "":
+                branch.capacity = None
+            else:
+                branch.capacity = int(capacity)
+            branch.save()
+            branch_id = branch.id
+            return HttpResponseRedirect(f'/banks/branch/{branch_id}/details/')
+
+    HttpResponseNotAllowed(['GET', 'POST'])
